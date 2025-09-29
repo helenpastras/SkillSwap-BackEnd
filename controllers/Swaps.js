8fdf812eda89f6d1471c3dccab07fdf2f2c6d5cb
helenpastras <helenapastras@hotmail.com>
2025-09-29 14:41:50 -0400

    test commit for PR experiments

---
+++ controllers/Swaps.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+router.get('/sign-boken', (req, res) => {
+  const user = {
+    _id: 1,
+    username: 'marco',
+    password: 'polo'
+  }
+
+  const token = jwt.sign({ user }, process.env.JWT_SECRET);
+  
+  res.json({ token });
+});
+
+// router.post('/verify-boken', (req, res) => {
+//   const boken = req.headers.authorization.split(' ')[1];
+//   try {
+//     const decoded = jwt.verify(boken, process.env.JWT_SECRET);
+    
+//     res.json({ decoded });
+//   } catch (err) {
+//     res.status(401).json({ err: 'Invalid boken.' });
+//   }
+// });
+
+module.exports = router;
